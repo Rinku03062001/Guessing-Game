8cf817910c2b3ce52d270fbf7e7e6d6fae293ea5
Rinku03062001 <160906758+Rinku03062001@users.noreply.github.com>
2024-09-23 22:39:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomnumber = random.Next(1, 100);
+
+            int maxAttempts = 5;
+            int attempts = 0;
+
+            while (attempts < maxAttempts)
+            {
+                Console.WriteLine("Enter your guess (1-100) : ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == randomnumber)
+                {
+                    Console.WriteLine("Congratulations, Guessing Successful!");
+                    break;
+                }
+                else if (guess > randomnumber)
+                {
+                    Console.WriteLine("Too high, Try Again");
+                }
+                else
+                {
+                    Console.WriteLine("Too low, Try Again");
+                }
+                attempts++;
+            }
+            if (attempts == maxAttempts)
+            {
+                Console.WriteLine("Game Over, The correct number is " + randomnumber);
+            }
+        }
+    }
+}
